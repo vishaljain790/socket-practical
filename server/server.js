7efa69f5f9d57844d24e4e7aae8918227f13ac71
Vishal_team <Vishal.j@myteam11.com>
2018-08-20 14:24:34 +0530

    chages console

--- server/server.js
+++ server/server.js
@@ -7,6 +7,6 @@ const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
 app.listen(3000, () => {
-    console.log(`server is running on port ${port}`);
+    console.log('server is running');
 });
 
