bc7d55c175457968b0da2e8ed472fa60365307f1
Vishal_team <Vishal.j@myteam11.com>
2018-08-20 14:29:18 +0530

    changes in port

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server is running');
 });
 
