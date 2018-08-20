97e25aa552363e409a67a4bd0b34f2f1bc4e090d
Vishal_team <Vishal.j@myteam11.com>
2018-08-20 15:56:46 +0530

    socket connection setup properly

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
 const app = express();
-
+const server = http.createServer(app);
+const socketIO = require('socket.io');
+var io = socketIO(server);
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log('server is running');
 });
 
